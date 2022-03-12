a61e26d757fbda265e65105e36a24ce1cefe85cb
shubhamp1997 <sp.shubham1997@gmail.com>
2022-03-12 06:18:45 -0500

    Front End + Java Main

--- app/src/main/java/com/shubham/fetchcodingtestapp/MainActivity.java
+++ app/src/main/java/com/shubham/fetchcodingtestapp/MainActivity.java
@@ -2,13 +2,112 @@ package com.shubham.fetchcodingtestapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.widget.ArrayAdapter;
+
+import com.shubham.fetchcodingtestapp.databinding.ActivityMainBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    ArrayList<String[]> itemList;
+    ArrayAdapter<String> listAdapter;
+    Handler mainHandler = new Handler();
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
+        initializeItemList();
+        new fetchData().start();
+    }
+
+    private void initializeItemList(){
+        itemList = new ArrayList<>();
+        listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, (List<String>) itemList);
+        binding.list.setAdapter(listAdapter);
+    }
+
+    class fetchData extends Thread{
+
+        String data = "";
+
+        @Override
+        public void run(){
+
+            mainHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    progressDialog = new ProgressDialog(MainActivity.this);
+                    progressDialog.setMessage("Retrieving Data");
+                    progressDialog.setCancelable(false);
+                    progressDialog.show();
+                }
+            });
+
+            try {
+                URL url = new URL("https://fetch-hiring.s3.amazonaws.com/hiring.json");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = connection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+
+                while ((line=reader.readLine())!=null){
+                    data = data + line;
+                }
+
+                if(!data.isEmpty()){
+
+                    JSONArray items = new JSONArray(data);
+
+                    for (int i = 0; i < items.length(); i++) {
+                        JSONObject item = items.getJSONObject(i);
+                        String listId = item.getString("listId");
+                        String id = item.getString("id");
+                        String name = item.getString("id");
+                        String[] itemArray ={listId + id + name};
+                        itemList.add(itemArray);
+
+                    }
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            mainHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    if(progressDialog.isShowing()){
+                        progressDialog.dismiss();
+                    }
+                }
+            });
+        }
     }
 }
