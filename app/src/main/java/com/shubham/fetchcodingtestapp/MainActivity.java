e84d266e74741db7bbe1481ff6c6a1cb77df8a46
shubhamp1997 <sp.shubham1997@gmail.com>
2022-03-13 03:22:33 -0400

    State clean

--- app/src/main/java/com/shubham/fetchcodingtestapp/MainActivity.java
+++ app/src/main/java/com/shubham/fetchcodingtestapp/MainActivity.java
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.HandlerThread;
-import android.view.View;
 import android.widget.ArrayAdapter;
 
 import com.shubham.fetchcodingtestapp.databinding.ActivityMainBinding;
@@ -15,28 +13,26 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
     ArrayList<String> itemArrayList;
     LinkedList<Item> itemList;
-    //ArrayList<String> idList;
-    //ArrayList<String> nameList;
-    //ArrayList<String> itemList4;
+    LinkedList<Item> itemList1;
+    LinkedList<Item> itemList2;
+    LinkedList<Item> itemList3;
+    LinkedList<Item> itemList4;
+
 
     ArrayAdapter<String> listAdapter;
     Handler mainHandler = new Handler();
@@ -48,17 +44,12 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         initializeItemList();
-        binding.fetchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new fetchData().start();
-            }
-        });
+        binding.fetchButton.setOnClickListener(view -> new fetchData().start());
         System.out.println("on create complete");
     }
 
     private void initializeItemList(){
-        itemList = new LinkedList<Item>();
+        itemList = new LinkedList<>();
         itemArrayList = new ArrayList<>();
         listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, (ArrayList<String>) itemArrayList);
         binding.itemsList.setAdapter(listAdapter);
@@ -72,15 +63,12 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run(){
 
-            mainHandler.post(new Runnable() {
-                @Override
-                public void run() {
-                    progressDialog = new ProgressDialog(MainActivity.this);
-                    progressDialog.setMessage("Retrieving Data");
-                    progressDialog.setCancelable(false);
-                    progressDialog.show();
-                    System.out.println("Showing progress dialog");
-                }
+            mainHandler.post(() -> {
+                progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setMessage("Retrieving Data");
+                progressDialog.setCancelable(false);
+                progressDialog.show();
+                System.out.println("Showing progress dialog");
             });
 
             try {
@@ -117,40 +105,30 @@ public class MainActivity extends AppCompatActivity {
                         String name = item.getString("name");
                         if(name.isEmpty()||name.equals("null"))
                             continue;
-                        //String[] itemArray ={listId + id + name};
-
-
 
                         itemList.add(new Item(listId,id,name));
 
-                        System.out.println("item "+i);
-
+                        if(listId>ids){
+                            ids = listId;
+                        }
                     }
+
+                    System.out.println("max id: "+ ids);
 
                     Collections.sort(itemList);
 
                     for (int i = 0; i < itemList.size(); i++) {
                         itemArrayList.add(itemList.get(i).itemDisc);
-                        System.out.println("Item array"+i);
                     }
-
                 }
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (JSONException e) {
+            } catch (IOException | JSONException e) {
                 e.printStackTrace();
             }
 
-            mainHandler.post(new Runnable() {
-                @Override
-                public void run() {
-                    if(progressDialog.isShowing()){
-                        progressDialog.dismiss();
-                        listAdapter.notifyDataSetChanged();
-                        System.out.println("Progress Dialog close");
-                    }
+            mainHandler.post(() -> {
+                if(progressDialog.isShowing()){
+                    progressDialog.dismiss();
+                    listAdapter.notifyDataSetChanged();
                 }
             });
         }
