541d22b30ac452b8d274a699e6030b55c1cbf680
shubhamp1997 <sp.shubham1997@gmail.com>
2022-03-13 23:05:28 -0400

    Lists Items Grouped

--- app/src/main/java/com/shubham/fetchcodingtestapp/MainActivity.java
+++ app/src/main/java/com/shubham/fetchcodingtestapp/MainActivity.java
@@ -27,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
     ArrayList<String> itemArrayList;
+    ArrayList<String> itemArrayList1;
+    ArrayList<String> itemArrayList2;
+    ArrayList<String> itemArrayList3;
+    ArrayList<String> itemArrayList4;
     LinkedList<Item> itemList;
     LinkedList<Item> itemList1;
     LinkedList<Item> itemList2;
@@ -34,7 +38,10 @@ public class MainActivity extends AppCompatActivity {
     LinkedList<Item> itemList4;
 
 
-    ArrayAdapter<String> listAdapter;
+    ArrayAdapter<String> listAdapter1;
+    ArrayAdapter<String> listAdapter2;
+    ArrayAdapter<String> listAdapter3;
+    ArrayAdapter<String> listAdapter4;
     Handler mainHandler = new Handler();
     ProgressDialog progressDialog;
 
@@ -43,17 +50,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        initializeItemList();
+        initializeItemList1();
+        initializeItemList2();
+        initializeItemList3();
+        initializeItemList4();
         binding.fetchButton.setOnClickListener(view -> new fetchData().start());
         System.out.println("on create complete");
     }
 
-    private void initializeItemList(){
-        itemList = new LinkedList<>();
-        itemArrayList = new ArrayList<>();
-        listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, (ArrayList<String>) itemArrayList);
-        binding.itemsList.setAdapter(listAdapter);
-        System.out.println("items List Initialized");
+    private void initializeItemList1(){
+        itemList1 = new LinkedList<>();
+        itemArrayList1 = new ArrayList<>();
+        listAdapter1 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, (ArrayList<String>) itemArrayList1);
+        binding.itemsList.setAdapter(listAdapter1);
+        System.out.println("items List 1 Initialized");
+    }
+
+    private void initializeItemList2(){
+        itemList2 = new LinkedList<>();
+        itemArrayList2 = new ArrayList<>();
+        listAdapter2 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, (ArrayList<String>) itemArrayList2);
+        binding.itemsList.setAdapter(listAdapter2);
+        System.out.println("items List 2 Initialized");
+    }
+
+    private void initializeItemList3(){
+        itemList3 = new LinkedList<>();
+        itemArrayList3 = new ArrayList<>();
+        listAdapter3 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, (ArrayList<String>) itemArrayList3);
+        binding.itemsList.setAdapter(listAdapter3);
+        System.out.println("items List 3 Initialized");
+    }
+
+    private void initializeItemList4(){
+        itemList4 = new LinkedList<>();
+        itemArrayList4 = new ArrayList<>();
+        listAdapter4 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, (ArrayList<String>) itemArrayList4);
+        binding.itemsList.setAdapter(listAdapter4);
+        System.out.println("items List 4 Initialized");
     }
 
     class fetchData extends Thread{
@@ -88,13 +122,17 @@ public class MainActivity extends AppCompatActivity {
 
                 }
 
+
                 if(!data.isEmpty()){
 
                     JSONArray items = new JSONArray(data);
 
                     System.out.println("items "+items.length());
 
-                    itemList.clear();
+                    itemList1.clear();
+                    itemList2.clear();
+                    itemList3.clear();
+                    itemList4.clear();
 
                     int ids = 0;
 
@@ -106,7 +144,16 @@ public class MainActivity extends AppCompatActivity {
                         if(name.isEmpty()||name.equals("null"))
                             continue;
 
-                        itemList.add(new Item(listId,id,name));
+                        switch (listId) {
+                            case 1:  itemList1.add(new Item(listId,id,name));
+                                break;
+                            case 2:  itemList2.add(new Item(listId,id,name));
+                                break;
+                            case 3:  itemList3.add(new Item(listId,id,name));
+                                break;
+                            case 4:  itemList4.add(new Item(listId,id,name));
+                                break;
+                        }
 
                         if(listId>ids){
                             ids = listId;
@@ -115,10 +162,22 @@ public class MainActivity extends AppCompatActivity {
 
                     System.out.println("max id: "+ ids);
 
-                    Collections.sort(itemList);
+                    Collections.sort(itemList1);
+                    Collections.sort(itemList2);
+                    Collections.sort(itemList3);
+                    Collections.sort(itemList4);
 
-                    for (int i = 0; i < itemList.size(); i++) {
-                        itemArrayList.add(itemList.get(i).itemDisc);
+                    for (int i = 0; i < itemList1.size(); i++) {
+                        itemArrayList1.add(itemList1.get(i).itemDisc);
+                    }
+                    for (int i = 0; i < itemList2.size(); i++) {
+                        itemArrayList2.add(itemList2.get(i).itemDisc);
+                    }
+                    for (int i = 0; i < itemList3.size(); i++) {
+                        itemArrayList3.add(itemList3.get(i).itemDisc);
+                    }
+                    for (int i = 0; i < itemList4.size(); i++) {
+                        itemArrayList4.add(itemList4.get(i).itemDisc);
                     }
                 }
             } catch (IOException | JSONException e) {
@@ -128,7 +187,10 @@ public class MainActivity extends AppCompatActivity {
             mainHandler.post(() -> {
                 if(progressDialog.isShowing()){
                     progressDialog.dismiss();
-                    listAdapter.notifyDataSetChanged();
+                    listAdapter1.notifyDataSetChanged();
+                    listAdapter2.notifyDataSetChanged();
+                    listAdapter3.notifyDataSetChanged();
+                    listAdapter4.notifyDataSetChanged();
                 }
             });
         }
